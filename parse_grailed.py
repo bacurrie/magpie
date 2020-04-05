efcbb4d9d426a082924056d4e54e280fa8da3ec0
bacurrie <bcurrie2@mta.ca>
2020-04-04 22:38:20 -0300

    added bs4 finds for grailed attributes

--- parse_grailed.py
+++ parse_grailed.py
@@ -1,16 +1,43 @@
 from bs4 import BeautifulSoup
 
-f = open('out.txt', 'w', encoding='utf-8')
-soup = BeautifulSoup(f, 'lxml')
+soup = BeautifulSoup(open('out.txt', 'r', encoding='utf-8'), 'lxml')
 
-#NEED attributes:
-#name
-#brand
-#size
-#old price
-#new price
-#date posted
-#date bumped
+# NEED attributes:
+# name
+    # <div class="listing-metadata"> --> <p class="truncate listing-title">
+# brand
+    # <div class="listing-metadata"> --> <>
+# size
+# old price
+# new price
+# date posted
+# date bumped
+# (later) origin site
 
+#TODO sizing guide for subscribers?
 
 for tag in soup.find_all("div", {"class": "feed-item"}):
+    print('==========')
+    title = tag.find('p', {"class": "truncate listing-title"})
+    designer = tag.find('p', {'class': 'listing-designer truncate'})
+    size = tag.find('p', {'class': 'listing-size sub-title'})
+    ogPrice = tag.find('p', {'class': 'sub-title original-price'})
+    oldPrice = tag.find('p', {'class': 'sub-title original-price strike-through'})
+    newPrice = tag.find('p', {'class': 'sub-title new-price'})
+    bump = tag.find('span', {'class': 'date-ago'})
+    origin = tag.find('span', {'class': 'bumped date-ago'})
+    
+    print(title.string)
+    print(designer.string)
+    print(size.string)
+
+    if ogPrice == None:
+        print("was: " + oldPrice.string)
+        print("now: " + newPrice.string)
+    else:
+        print(ogPrice.string)
+    if origin == None:
+        print("posted " + bump.string)
+    else:
+        print("bumped " + bump.string)
+        print("listed " + origin.string)
